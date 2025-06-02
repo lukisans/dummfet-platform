0f00dc251a7f60389a94d4bf49181581d8032afa
lukisans <fahmilukis@outlook.com>
2025-06-02 13:44:33 +0700

    change backbone to official react router template

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -2,12 +2,10 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function formatAddress(address: string | undefined) {
-  if (!address || address.length < 10) {
-    throw new Error("Invalid wallet address");
-  }
-  return `${address.slice(0, 6)}...${address.slice(-4)}`;
+    if (!address || address.length < 10) { throw new Error("Invalid wallet address"); } return `${address.slice(0,
+        6)}...${address.slice(-4)}`;
 }
